3d391d71b4bc2ddc9fc9154d0741fe1681103a08
Christian Fobel <christian@fobel.net>
2012-06-29 11:00:09 -0400

    Add setup.py to allow installation using pip, etc.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "tex-helper",
+    version = "0.01",
+    description = "A helper package for LaTex tables, and figures.",
+    keywords = "latex tex table figure",
+    author = "Christian Fobel",
+    url = "https://github.com/cfobel/python___tex_helper",
+    license = "LGPL",
+    long_description = """""",
+    packages = ['tex_helper']
+)
